a4c67c1a02729bf0655857351d1a3b3e2db07dd5
HumbleHunger <2495970924@qq.com>
2021-11-30 18:42:51 +0800

    s

---
+++ 55.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+			int cover = 0;
+
+			for (int i = 0; i <= cover; ++i) {
+				cover = max(i + nums[i], cover);
+				if (cover >= nums.size() - 1) return true;
+			}
+			return false;
+    }
+};
