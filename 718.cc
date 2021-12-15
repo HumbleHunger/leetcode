300fe6f85d1e450171f081e952fa465336763b80
HumbleHunger <2495970924@qq.com>
2021-12-15 21:14:12 +0800

    s

--- 718.cc
+++ 718.cc
@@ -5,6 +5,18 @@ using namespace std;
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-			
+		vector<int> dp(nums2.size() + 1, 0);
+
+        int ret = 0;
+        for (int i = 1; i <= nums1.size(); ++i) {
+            for (int j = nums2.size(); j > 0; --j) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = dp[j - 1] + 1;
+                } else dp[j] = 0;
+                ret = max(ret, dp[j]);
+            }
+        }
+
+        return ret;
     }
 };
