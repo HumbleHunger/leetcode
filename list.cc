ec0bd78b11f9c5df17bd6957c39fef24ccbfde96
HumbleHunger <2495970924@qq.com>
2022-02-23 04:06:19 +0800

    s

---
+++ list.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	list<int> l;
+	map<int, int> m;
+	
+}
--- offer12.cc
+++ offer12.cc
@@ -4,36 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    bool f(vector<vector<char>>& board, string word, int i, int j, char index) {
-			if (index >= word.size()) return true;
-			int ret = false;
-			// 上
-			if (i > 0 && board[i - 1][j] == word[index]) {
-				ret = f(board, word, i - 1, j, index + 1);
-			}
-			// 下
-			if (i < board.size() - 1 && board[i + 1][j] == word[index]) {
-				ret = f(board, word, i + 1, j, index + 1);
-			}
-			// 左
-			if (j > 0 && board[i][j - 1] == word[index]) {
-				ret = f(board, word, i, j - 1, index + 1);
-			}
-			// 右
-			if (j < board.size() - 1 && board[i][j + 1] == index) {
-				ret = f(board, word, i, j + 1, index + 1);
-			}
-			return ret;
-		}
-
     bool exist(vector<vector<char>>& board, string word) {
-			int i, j;
-			int m = 0;
-			for (i = 0; i < board.size(); ++i) {
-				for (j = 0; j < board[0].size(); ++j) {
-					if (board[i][j] == word[0]) { m = 0; break; }
-				}
-			}
-			if (m) f(board, word, i, j, 1);
+        rows = board.size();
+        cols = board[0].size();
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+private:
+    int rows, cols;
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int k) {
+        if(i >= rows || i < 0 || j >= cols || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.size() - 1) return true;
+        board[i][j] = ' ';
+        bool res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i, j + 1, k + 1) || 
+                      dfs(board, word, i - 1, j, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = word[k];
+        return res;
     }
 };
